e9fab1b0a8d186108eec4367d6aa8458c5244b52
scl <chunlei.song@live.com>
2017-12-27 10:52:13 +0800

    基础模块调整

---
+++ grimlock-admin-web/src/main/java/org/grimlock/admin/web/AdminWebApp.java
@@ -0,0 +1,11 @@
+package org.grimlock.admin.web;
+
+/**
+ * @Author:chunlei.song@live.com
+ * @Description:
+ * @Date Create in 10:38 2017-12-22
+ * @Modified By:
+ */
+public class AdminWebApp {
+
+}
